6e8777a9d39e95e34237acde36c30f232e126c10
CansuCulfa <cansupelin4@gmail.com>
2022-06-12 12:38:55 +0300

    User Profile

---
+++ resources/views/home/user/user_profile.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.frontbase')
+
+@section('title', 'User Profile')
+
+@section('content')
+    <!--  BREADCUMB -->
+<div id="breadcrumb">
+    <div class="container">
+        <ul class="breadcrumb">
+            <li><a href="{{route('home')}}">Home</a></li>
+            <li class="active">User Profile</li>
+        </ul>
+    </div>
+</div>
+<!--  /BREADCUMB -->
+
+<!--  section-->
+<div class="section">
+    <!--  container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+
+
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container-->
+</div>
+@endsection
